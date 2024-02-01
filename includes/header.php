6db46bdeff8141462312afa34661cfd531d94b68
Kyle J. Harms <kyle.harms@cornell.edu>
2024-02-01 12:58:18 -0500

    lab2 materials

---
+++ includes/header.php
@@ -0,0 +1,10 @@
+<header>
+  <h1 id="title"><?php echo $page_title; ?> - INFO 2300</h1>
+
+  <nav id="menu">
+    <ul>
+      <li class="active_page"><a href="index.php">Home</a></li>
+      <li class=""><a href="citations.php">Citations</a></li>
+    </ul>
+  </nav>
+</header>
